dc048b3339a3576a435bac368347cf5dc514da71
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2022-05-28 12:37:23 -0400

    deleted sudoku.c renamed sudoku2.c to sudoku.c, also did implementation spec

---
+++ IMPLEMENTATION.md
@@ -0,0 +1,174 @@
+# Detailed pseudo code for each of the functions:
+
+`int main(int argc, char const *argv[])`
+```
+returns nonzero and prints error if the the wrong number of command-line arguments are given
+if first and only argument is "create",
+    sets up random number system using the current time
+    generates a puzzle using generatePuzzle()
+    returns 0
+if first and only argument is "solve",
+    creates a 9x9 2d array to store the puzzle
+    for each x value 0 to 8 in the grid,
+        for each y value 0 to 8 in the grid,
+            attempts to scan an integer from stdin and if fails,
+                tries to scan another integer from stdin
+    creates a bool initially set to false
+    solves puzzle, using the address of the bool to keep track of how many solutions have been printed
+    returns 0
+returns nonzero and prints error for incorrect arguments
+```
+
+# Pseudocode used in both `./sudoku create` and `./sudoku solve`
+
+`bool possible(int (*arr)[9], int y, int x, int num)`
+```
+for each array index in the row at x,y
+    if the array element at the given y value and the row index is equal to the number specified,
+        return false
+for each array index in the column at x,y
+    if the array element at the given x value and the column index is equal to the number specified,
+        return false
+creates ints x0 and y0 which are x and y rounded down to the nearest multiple of 3
+for i 0 to 2
+    for j 0 to 2
+        if the array element at (x0 + j, y0 + i) is equal to the number specified,
+            return false
+return true
+```
+
+# Pseudocode for functions only used for `./sudoku create`:
+
+`void solutionCounter(int (*arr)[9], int *solutions)`
+```
+if value at solutions pointer is at least 2,
+    return early
+
+for y 0 to 8
+    for x 0 to 8
+        if the array element at x,y is 0,
+            for n 1 to 9
+                if n is possible at the current point x,y in the array,
+                    sets the array element at x,y to n
+                    runs solutionCounter again
+                    sets array element at x,y back to 0
+            return early
+adds 1 to value at solutions pointer
+```
+
+`void generatePuzzle()`
+```
+creates empty sudoku array
+sets all elements in 2d array to zero
+for each point x,y in the top-left 3x3 square in the array
+    while true,
+        generates random number 1 to 9
+        if the number is possible at x,y,
+            insert it
+            break out of while loop
+for each point x,y in the middle 3x3 square in the array
+    while true,
+        generates random number 1 to 9
+        if the number is possible at x,y,
+            insert it
+            break out of while loop
+for each point x,y in the bottom-right 3x3 square in the array
+    while true,
+        generates random number 1 to 9
+        if the number is possible at x,y,
+            insert it
+            break out of while loop
+creates a bool initially set to false
+uses generatePuzzleHelper to solve the puzzle and then remove values from it, passing in the address to the bool
+```
+
+`void generatePuzzleHelper(int (*arr)[9], bool *created)`
+```
+if value at bool pointer given is true,
+    returns early
+for y 0 to 8
+    for x 0 to 8
+        if the array element at x,y is 0,
+            for n 1 to 9
+                if n is possible at the current point x,y in the array,
+                    sets the array element at x,y to n
+                    runs generatePuzzleSolver again
+                    sets array element at x,y back to 0
+            return early
+sets value at created equal to true
+creates an int "times" initially zero to represent amount of numbers removed
+creates an int maxTimes between 40 and 54 to represent amount of numbers to remove
+while times is less than or equal to maxTimes,
+    creates two random x and y ints 1 to 9
+    if the array element at x,y is 0
+        continue; don't do anything else
+    creates temp int to store the current value in the array at x,y
+    sets array value at x,y to 0
+    makes a copy of the current array
+    uses solutionCounter to count number of solutions in the copy
+    if the number of solutions is not 1,
+        sets the array value at x,y back to the temp value it was before set to 0
+        continue; don't do anything else
+    increment times
+print the 2d sudoku array
+```
+
+# Pseudocode for function used in `./sudoku solve`
+
+`void solve(int (*arr)[9], bool *printedOneSolution)`
+```
+if value at printedOneSolution pointer is true,
+    return early
+
+for y 0 to 8
+    for x 0 to 8
+        if the array element at x,y is 0,
+            for n 1 to 9
+                if n is possible at the current point x,y in the array,
+                    sets the array element at x,y to n
+                    runs solve again
+                    sets array element at x,y back to 0
+            return early
+sets value at printedOneSolution equal to true
+prints solved sudoku grid
+```
+
+# Detailed APIs used:
+## from `stdlib.h`:
+```c
+/**************** rand ****************/
+/*
+ * returns the next pseudo-random number in the series. The range value of the 
+ * number series is between 0 and RAND_MAX. RAND_MAX is a macro defined in stdlib.h 
+ * header file, whose value is the maximum value, which can return by rand() function. 
+ * The value of RAND_MAX is greater but not less than 32767 depending on the C libraries
+ */
+int rand();
+
+/**************** srand ****************/
+/*
+ * seeds the random number generator used by the function rand.
+ */
+void srand(unsigned int seed);
+```
+
+## from `time.h`:
+```c
+/**************** time ****************/
+/*
+ * returns the time since 00:00:00 UTC, January 1, 1970 (Unix timestamp) in seconds. 
+ * If second is not a null pointer, the returned value is also stored in the object 
+ * pointed to by second. Used for our purposes to seed the random number generator based
+ * on the current time so that each time program runs it generates different numbers
+ */
+time_t time( time_t *second );
+
+```
+#
+
+# Data structures 
+the only data structure used in *sudoku.c* is an `int[9][9]` to represent the sudoku puzzle.
+
+when `./sudoku create` is run it creates an `int[9][9]` initially filled with zeros, solves it, and removes some numbers.
+
+when `./sudoku solve` is run, 81 numbers are read in from stdin and stored into an `int[9][9]` and it is then solved and printed.
--- sudoku.c
+++ sudoku.c
@@ -27,7 +27,7 @@ bool possible(int (*arr)[9], int y, int x, int num);
  * grid at the end
  * exits execution at the end of the function so that recursion stops solving other solutions
  */
-void solve(int (*arr)[9], int seed, bool generating);
+void solve(int (*arr)[9], bool *printedOneSolution);
 
 /**
  * printGrid:
@@ -46,6 +46,8 @@ void generatePuzzle();
 
 void solutionCounter(int (*arr)[9], int *solutions);
 
+void generatePuzzleHelper(int (*arr)[9], bool *created);
+
 int main(int argc, char const *argv[])
 {
     if (argc != 2)
@@ -62,7 +64,6 @@ int main(int argc, char const *argv[])
     }
     if (strcmp(argv[1], "solve") == 0)
     {
-        srand(time(NULL));
         int a[9][9];
         for (int x = 0; x < 9; x++)
         {
@@ -74,7 +75,8 @@ int main(int argc, char const *argv[])
                 }
             }
         }
-        solve(a, rand(), false);
+        bool printed = false;
+        solve(a, &printed);
         return 0;
     }
 
@@ -124,70 +126,13 @@ bool possible(int (*arr)[9], int y, int x, int num)
     return true;
 }
 
-void solve(int (*arr)[9], int seed, bool generating)
-{
-    int start = seed % 9; // random num 1 to 9 start
-    int y = start;
-    do
-    {
-        for (int x = 0; x < 9; x++)
-        {
-            if (arr[y][x] == 0)
-            {
-                for (int n = 1; n < 10; n++)
-                {
-                    if (possible(arr, y, x, n))
-                    {
-                        arr[y][x] = n;
-                        solve(arr, seed, generating);
-                        arr[y][x] = 0;
-                    }
-                }
-                return;
-            }
-        }
-        y = (y >= 8) ? 0 : y + 1;
-    } while (y != start);
-
-    if (generating)
-    {
-        int maxTimes = 40 + rand() % 24;
-        for (int times = 0; times < maxTimes; times++)
-        {
-            int x = rand() % 9;
-            int y = rand() % 9;
-            if (arr[y][x] != 0)
-            {
-                int temp = arr[y][x];
-                arr[y][x] = 0;
-
-                int b[9][9];
-                for (int x0 = 0; x0 < 9; x0++)
-                {
-                    for (int y0 = 0; y0 < 9; y0++)
-                    {
-                        b[y0][x0] = arr[y0][x0];
-                    }
-                }
-
-                int solutions = 0;
-                solutionCounter(b, &solutions);
-                printf("solutions: %d\n", solutions);
-                if (solutions != 1)
-                {
-                    arr[y][x] = temp;
-                    times--;
-                }
-                continue;
-            }
-            times--;
-        }
-    }
-    printf("printing the thing\n");
-    printGrid(arr);
-}
-
-/* old solve before I made it sus */
+/**
+ * solutionCounter:
+ * solves a given sudoku grid, and each time solved adds to
+ * an int at a pointer passed in which keeps track of how many solutions there are
+ * so that we can use this while creating a new puzzle that must have a unique solution
+ * each recursive call only runs if there have been less than 2 solutions found
+ */
 void solutionCounter(int (*arr)[9], int *solutions)
 {
     if (*solutions >= 2)
@@ -214,10 +159,39 @@ void solutionCounter(int (*arr)[9], int *solutions)
     *solutions += 1;
 }
 
-// check if puzzle is solvable and has unique solution 
-//      use solve function to see if it has unique solution
-//      keep count of how many solutions can be found
-//      if count > 1 return false 
+void solve(int (*arr)[9], bool *printedOneSolution)
+{
+    if (*printedOneSolution)
+        return;
+    for (int y = 0; y < 9; y++)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            if (arr[y][x] == 0)
+            {
+                for (int n = 1; n < 10; n++)
+                {
+                    if (possible(arr, y, x, n))
+                    {
+                        arr[y][x] = n;
+                        solve(arr, printedOneSolution);
+                        arr[y][x] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+    *printedOneSolution = true;
+    printGrid(arr);
+}
+
+/**
+ * generatePuzzle:
+ * creates an empty sudoku grid and places numbers in the 3 squares
+ * diagonally from top left to bottom right. Then calls generatePuzzleHelper
+ * on the grid, which solves it and takes numbers out.
+ */
 void generatePuzzle()
 {
     int a[9][9];
@@ -228,10 +202,120 @@ void generatePuzzle()
             a[y][x] = 0;
         }
     }
-    int x = rand() % 9;
-    int y = rand() % 9;
-    int n = rand() % 9 + 1;
-    a[y][x] = n;
-    int random = rand();
-    solve(a, random, true);
+
+    // sets three diagonal squares of 9 numbers all to random numbers
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            while (true)
+            {
+                int trying = rand() % 9 + 1;
+                if (possible(a, y, x, trying))
+                {
+                    a[y][x] = trying;
+                    break;
+                }
+            }
+        }
+    }
+    for (int x = 3; x < 6; x++)
+    {
+        for (int y = 3; y < 6; y++)
+        {
+            while (true)
+            {
+                int trying = rand() % 9 + 1;
+                if (possible(a, y, x, trying))
+                {
+                    a[y][x] = trying;
+                    break;
+                }
+            }
+        }
+    }
+    for (int x = 6; x < 9; x++)
+    {
+        for (int y = 6; y < 9; y++)
+        {
+            while (true)
+            {
+                int trying = rand() % 9 + 1;
+                if (possible(a, y, x, trying))
+                {
+                    a[y][x] = trying;
+                    break;
+                }
+            }
+        }
+    }
+
+    // printf("generated diagonal matrices:\n");
+    // printGrid(a);
+
+    bool createdPuzzle = false;
+    generatePuzzleHelper(a, &createdPuzzle); // solves the puzzle, then removes a maximum of 54 numbers from it, then prints the puzzle!
+}
+
+void generatePuzzleHelper(int (*arr)[9], bool *created)
+{
+    if (*created)
+        return;
+    for (int y = 0; y < 9; y++)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            if (arr[y][x] == 0)
+            {
+                for (int n = 1; n < 10; n++)
+                {
+                    if (possible(arr, y, x, n))
+                    {
+                        arr[y][x] = n;
+                        generatePuzzleHelper(arr, created);
+                        arr[y][x] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+    *created = true;
+    // printf("Puzzle solved:\n");
+    // printGrid(arr);
+    int times = 0;
+    int maxTimes = 40 + rand() % 15;
+    while (times <= maxTimes)
+    {
+        // printf("Grid with %d numbers removed:\n", times);
+        // printGrid(arr);
+        // printf("\n");
+        int x = rand() % 9;
+        int y = rand() % 9;
+        if (arr[y][x] == 0)
+            continue;
+
+        int temp = arr[y][x];
+        arr[y][x] = 0;
+
+        int b[9][9];
+        for (int x0 = 0; x0 < 9; x0++)
+        {
+            for (int y0 = 0; y0 < 9; y0++)
+            {
+                b[y0][x0] = arr[y0][x0];
+            }
+        }
+
+        int solutions = 0;
+        solutionCounter(b, &solutions);
+        // printf("solutions: %d\n", solutions);
+        if (solutions != 1)
+        {
+            arr[y][x] = temp;
+            continue;
+        }
+        times++;
+    }
+    printGrid(arr);
 }
