6408ca17dea7eaeb128bddfdb30bebaa4f248fe9
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2022-05-29 14:30:54 -0400

    added some js functoins

--- colorsudoku/src/App.js
+++ colorsudoku/src/App.js
@@ -14,6 +14,20 @@ var grid = [
     [0, 0, 0, 0, 3, 0, 0, 7, 0]
 ];
 
+function newPuzzle() {
+    return [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+}
+
 function possible(arr, x, y, num) {
     for (let i = 0; i < 9; i++) {
         if (arr[i][y] === num)
@@ -34,6 +48,154 @@ function possible(arr, x, y, num) {
     return true;
 }
 
+function solve(arr, printedOneSolution)
+{
+    if (printedOneSolution.bool)
+        return;
+    for (let y = 0; y < 9; y++)
+    {
+        for (let x = 0; x < 9; x++)
+        {
+            if (arr[y][x] == 0)
+            {
+                for (let n = 1; n < 10; n++)
+                {
+                    if (possible(arr, x, y, n))
+                    {
+                        arr[x][y] = n;
+                        solve(arr, printedOneSolution);
+                        arr[x][y] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+    printedOneSolution.bool = true;
+    printGrid(arr);
+}
+
+
+function generatePuzzle()
+{
+    let a = newPuzzle();
+
+    // sets three diagonal squares of 9 numbers all to random numbers
+    for (let x = 0; x < 3; x++)
+    {
+        for (let y = 0; y < 3; y++)
+        {
+            while (true)
+            {
+                let trying = rand() % 9 + 1;
+                if (possible(a, y, x, trying))
+                {
+                    a[y][x] = trying;
+                    break;
+                }
+            }
+        }
+    }
+    for (let x = 3; x < 6; x++)
+    {
+        for (let y = 3; y < 6; y++)
+        {
+            while (true)
+            {
+                let trying = rand() % 9 + 1;
+                if (possible(a, y, x, trying))
+                {
+                    a[y][x] = trying;
+                    break;
+                }
+            }
+        }
+    }
+    for (let x = 6; x < 9; x++)
+    {
+        for (let y = 6; y < 9; y++)
+        {
+            while (true)
+            {
+                let trying = rand() % 9 + 1;
+                if (possible(a, x, y, trying))
+                {
+                    a[x][y] = trying;
+                    break;
+                }
+            }
+        }
+    }
+
+    // printf("generated diagonal matrices:\n");
+    // printGrid(a);
+
+    let bool = { created: false };
+    generatePuzzleHelper(a, bool); // solves the puzzle, then removes a maximum of 54 numbers from it, then prints the puzzle!
+}
+
+function generatePuzzleHelper(arr, bool)
+{
+    if (bool.created)
+        return;
+    for (let y = 0; y < 9; y++)
+    {
+        for (let x = 0; x < 9; x++)
+        {
+            if (arr[x][y] == 0)
+            {
+                for (let n = 1; n < 10; n++)
+                {
+                    if (possible(arr, x, y, n))
+                    {
+                        arr[x][y] = n;
+                        generatePuzzleHelper(arr, bool);
+                        arr[x][y] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+    bool.created = true;
+    // printf("Puzzle solved:\n");
+    // printGrid(arr);
+    let times = 0;
+    let maxTimes = 40 + rand() % 15;
+    while (times <= maxTimes)
+    {
+        // printf("Grid with %d numbers removed:\n", times);
+        // printGrid(arr);
+        // printf("\n");
+        let x = rand() % 9;
+        let y = rand() % 9;
+        if (arr[x][y] == 0)
+            continue;
+
+        let temp = arr[x][y];
+        arr[x][y] = 0;
+
+        let b = newPuzzle;
+        for (let x0 = 0; x0 < 9; x0++)
+        {
+            for (let y0 = 0; y0 < 9; y0++)
+            {
+                b[x0][y0] = arr[x0][y0];
+            }
+        }
+
+        let int = {solutions: 0};
+        solutionCounter(b, int);
+        // printf("solutions: %d\n", solutions);
+        if (int.solutions != 1)
+        {
+            arr[x][y] = temp;
+            continue;
+        }
+        times++;
+    }
+    printGrid(arr);
+}
 
 
 function App() {
